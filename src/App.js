d8df9a9d272cc98162ef4de615fc89cb081e4c01
Nick Dolan-Stern <sirocknick@gmail.com>
2021-05-02 13:24:13 -0700

    Pokemon list is now passed as props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./styles.css";
 import Pokedex from "./Section5/Pokedex";
-export class App extends React.Component {
+import React, { Component } from "react";
+
+export class App extends Component {
   render() {
-    return <Pokedex />;
+    const pokemon = [
+      { id: 4, name: "Charmander", type: "Fire", base_experience: 62 },
+      { id: 7, name: "Squirtle", type: "Water", base_experience: 63 },
+      { id: 11, name: "Metapod", type: "Bug", base_experience: 72 },
+      { id: 12, name: "Butterfree", type: "Flying", base_experience: 178 },
+      { id: 25, name: "Pikachu", type: "Electric", base_experience: 112 },
+      { id: 39, name: "Jigglypuff", type: "Normal", base_experience: 95 },
+      { id: 94, name: "Gengar", type: "Poison", base_experience: 225 },
+      { id: 133, name: "Eevee", type: "Normal", base_experience: 65 }
+    ];
+    return <Pokedex pokemon={pokemon} />;
   }
 }
--- src/Section5/Pokedex.js
+++ src/Section5/Pokedex.js
@@ -3,22 +3,11 @@ import Pokecard from "./Pokecard";
 import "./Pokedex.css";
 
 class Pokedex extends Component {
-  get pokemon() {
-    return [
-      { id: 4, name: "Charmander", type: "Fire", base_experience: 62 },
-      { id: 7, name: "Squirtle", type: "Water", base_experience: 63 },
-      { id: 11, name: "Metapod", type: "Bug", base_experience: 72 },
-      { id: 12, name: "Butterfree", type: "Flying", base_experience: 178 },
-      { id: 25, name: "Pikachu", type: "Electric", base_experience: 112 },
-      { id: 39, name: "Jigglypuff", type: "Normal", base_experience: 95 },
-      { id: 94, name: "Gengar", type: "Poison", base_experience: 225 },
-      { id: 133, name: "Eevee", type: "Normal", base_experience: 65 }
-    ];
-  }
   render() {
-    const listItems = this.pokemon.map((p) => (
+    const listItems = this.props.pokemon.map((p, key) => (
       <Pokecard
         className="Pokedex"
+        key={key}
         id={p.id}
         name={p.name}
         type={p.type}
