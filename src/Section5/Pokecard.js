fc48cb8a4749e651715dd1b104b818caf581b33d
Nick Dolan-Stern <sirocknick@gmail.com>
2021-05-02 13:19:28 -0700

    Improved Pictures

--- src/Section5/Pokecard.js
+++ src/Section5/Pokecard.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
 import "./Pokecard.css";
 
+function numpad(num) {
+  let string = num.toString();
+  while (string.length < 3) {
+    string = "0" + string;
+  }
+  return string;
+}
+
 class Pokecard extends Component {
   render() {
-    const pokeImg = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.props.id}.png`;
+    const pokeImg = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${numpad(
+      this.props.id
+    )}.png`;
     const cardClass = `Pokecard ${this.props.type}`;
     const imgClass = `Pokecard-img ${this.props.type}-img`;
     return (
@@ -13,7 +23,6 @@ class Pokecard extends Component {
         </header>
         <img src={pokeImg} className={imgClass} alt={this.props.name} />
         <footer className="Pokecard-footer">
-          <span className="Pokecard-type">{this.props.type} Type</span>
           <span className="Pokecard-exp">{this.props.exp} XP </span>
         </footer>
       </div>
